b7432cbc64581be5af9a30fdecc4f0726341df94
Eltaj Gafarli <eltacqeferli@gmail.com>
2024-09-01 04:12:34 +0400

    updated OpenApiConfigs

--- src/main/java/com/example/astrotalk/util/OpenApiConstants.java
+++ src/main/java/com/example/astrotalk/util/OpenApiConstants.java
@@ -9,7 +9,7 @@ public final class OpenApiConstants {
             " This application is designed to be easily integrated into your projects, " +
             "offering a secure and seamless authentication process. ";
     public static final String VERSION = "1.0";
-    public static final String TITLE = "CBAR";
+    public static final String TITLE = "AstroTalks";
 
     private OpenApiConstants() {
     }
