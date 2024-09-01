8e7592c62e761f2db0f4fa60fdf3bfb8ea4c8eb0
Eltaj Gafarli <eltacqeferli@gmail.com>
2024-09-01 04:21:56 +0400

    updated OpenApiConfigs

--- src/main/java/com/example/astrotalk/util/OpenApiConstants.java
+++ src/main/java/com/example/astrotalk/util/OpenApiConstants.java
@@ -4,10 +4,9 @@ public final class OpenApiConstants {
     public static final String SECURITY_SCHEME_NAME = "BearerAuth";
     public static final String SCHEME = "bearer";
     public static final String BEARER_FORMAT = "JWT";
-    public static final String DESCRIPTION = "A robust Spring Boot authentication application that provides user registration," +
-            " login, password reset, and email confirmation functionalities." +
-            " This application is designed to be easily integrated into your projects, " +
-            "offering a secure and seamless authentication process. ";
+    public static final String DESCRIPTION = "AstroTalks is a feature-rich platform designed to connect users with professional astrologers for personalized consultations. " +
+            "The application offers seamless appointment booking, along with daily horoscopes and personalized astrological reports. " +
+            "AstroTalks is designed to be easily integrated into various projects, providing users with an engaging and intuitive astrological experience.";
     public static final String VERSION = "1.0";
     public static final String TITLE = "AstroTalks";
 
