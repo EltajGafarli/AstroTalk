56d0d136e887597fae6653a6000f480e08a4e91b
Eltaj Gafarli <eltacqeferli@gmail.com>
2024-09-16 02:26:28 +0400

    serialization bug fixed for motvation request dto

--- src/main/java/com/example/astrotalk/dto/MotivationRequestDto.java
+++ src/main/java/com/example/astrotalk/dto/MotivationRequestDto.java
@@ -1,8 +1,12 @@
 package com.example.astrotalk.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class MotivationRequestDto {
